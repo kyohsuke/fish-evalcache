b0e76150ce55a6e4374276faed6dc06603efec87
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-09-29 22:19:29 +0900

    Fix export

--- conf.d/_evalcache_clear.fish
+++ conf.d/_evalcache_clear.fish
@@ -1,6 +1,6 @@
 function _evalcache_clear
   if test -z "$FISH_EVALCACHE_DIR"
-    export FISH_EVALCACHE_DIR="$HOME/.fish-evalcache"
+    set -gx FISH_EVALCACHE_DIR "$HOME/.fish-evalcache"
   end
 
   rm -i "$FISH_EVALCACHE_DIR"/init-*.sh
