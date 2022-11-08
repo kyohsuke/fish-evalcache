7f4a404871b7017abe8386f4ff05fca2ff7ed41e
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-11-09 02:20:25 +0900

    Add more tests

---
+++ tests/_evalcache_clear.fish
@@ -0,0 +1,12 @@
+@echo === _evalcache_clear ===
+
+set resultMD5 (echo -n "./testeval" | md5sum | cut -d' ' -f1)
+set testFile "$HOME/.fish-evalcache/init-testeval-$resultMD5.fish"
+
+echo "testtesttest" > "$testFile"
+
+@test "cache file exists" -f "$testFile"
+
+@test "cache file could remove" (echo -n "y" | _evalcache_clear) $status -eq 0
+
+@test "cache file does not exists" ! -f "$testFile"
