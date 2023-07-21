5a829b06aff23352e32e90220428145d82605af0
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2023-07-21 23:40:21 +0900

    Add _evalcache_clear somoecommand

--- tests/_evalcache_clear.fish
+++ tests/_evalcache_clear.fish
@@ -7,6 +7,24 @@ echo testtesttest >"$testFile"
 
 @test "cache file exists" -f "$testFile"
 
-@test "cache file could remove" (echo -n "y" | _evalcache_clear) $status -eq 0
+@test "all cache file could remove" (echo -n "y" | _evalcache_clear) $status -eq 0
 
 @test "cache file does not exists" ! -f "$testFile"
+
+@echo === _evalcache_clear $testFile ===
+
+set resultMD5 (echo -n "./testeval" | md5sum | cut -d' ' -f1)
+set testFile "$HOME/.fish-evalcache/init-testeval-$resultMD5.fish"
+echo testtesttest >"$testFile"
+
+set resultMD5_2 (echo -n "./testeval2" | md5sum | cut -d' ' -f1)
+set testFile2 "$HOME/.fish-evalcache/init-testeval2-$resultMD5_2.fish"
+echo testtesttest2 >"$testFile2"
+
+@test "cache file exists" -f "$testFile"
+@test "cache file exists" -f "$testFile2"
+
+@test "target cache file could remove" (echo -n "y" | _evalcache_clear testeval) $status -eq 0
+
+@test "cache file does not exists" ! -f "$testFile"
+@test "cache file exists" -f "$testFile2"
